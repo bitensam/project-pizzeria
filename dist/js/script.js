c079ddf642fa79065a5daddd8659840f30ef73cb
Jakub Wojcik <j.wojcik1994@gmail.com>
2021-05-19 23:29:59 +0200

    Add function initAccordion and renderInMenu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
 {
@@ -52,16 +54,105 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  /* MODULE 7.4 - making a new class Product */
+
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+    }
+
+    // module 7.5
+    renderInMenu() {
+      const thisProduct = this;
+
+      /* GENERATE HTML BASED ON TEMPLATE */
+
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      /* CREATE ELEMENT USING UTILS.CREATEELEMENTFROMHTML */
+
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* FIND MENU CONTAINER */
+
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* ADD ELEMENT TO MENU */
+
+      menuContainer.appendChild(thisProduct.element);
+
+      console.log('new Product', thisProduct);
+    }
+
+    // module 7.5
+    initAccordion() {
+
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      /* START: add event listener to clickable trigger on event click */
+
+      clickableTrigger.addEventListener('click', function (event) {
+
+        /* prevent default action for event */
+
+        event.preventDefault();
+
+        /* find active product (product that has active class) */
+
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+
+        if (activeProduct && activeProduct !== thisProduct.element) {
+          activeProduct.classList.remove('active');
+
+        }
+
+        /* toggle active class on thisProduct.element */
+
+        thisProduct.element.classList.toggle('active');
+      });
+
+    }
+  }
+
   const app = {
-    init: function(){
+    initMenu: function () {
+
+      const thisApp = this;
+
+      console.log('thisApp.data', thisApp.data);
+
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
+    },
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
     },
   };
-
   app.init();
 }
