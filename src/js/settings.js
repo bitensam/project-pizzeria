2294ef9979476ecc8c698ebe93ec2c6da7ccccbb
Jakub Wojcik <j.wojcik1994@gmail.com>
2021-06-08 17:52:35 +0200

    test

--- src/js/settings.js
+++ src/js/settings.js
@@ -111,7 +111,7 @@ export const settings = {
   },
   db: {
     url: '//localhost:3131',
-    product: 'product',
+    products: 'products',
     order: 'order',
     booking: 'booking',
     event: 'event',
